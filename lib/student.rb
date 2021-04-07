66925e72d6cee3adcdbdf40578adeffd19ca64a1
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-07 22:05:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,8 +44,8 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
-  def self.create(name:, :grade)
-    student = self.new(name, grade)
+  def self.create(name, grade)
+    student = self.new(name:, grade:)
     student.save
     student
   end
