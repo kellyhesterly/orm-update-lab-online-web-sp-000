281ad6c302689cc6fff82a2f5a4ee939e7154738
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-07 22:03:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,5 +38,9 @@ class Student
   end
 
   def update
+    sql = <<-SQL 
+    UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL 
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 end
