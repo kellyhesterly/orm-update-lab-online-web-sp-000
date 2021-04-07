7e857cee128ec5825fbf1eb8b9c0e1041c37e281
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-07 22:08:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new(row)[0]
+    student = self.new(name, grade, id = nil)
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
